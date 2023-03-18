0ce66d1de7337452c786f7f9c34dc39c485dc8f4
juanm <juanm@JUANMA>
2023-03-17 19:23:36 -0500

    Crude

---
+++ ECommerce_Sat/Controllers/CountriesController.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ECommerce_Sat.DAL;
+using ECommerce_Sat.DAL.Entities;
+
+namespace ECommerce_Sat.Controllers
+{
+    public class CountriesController : Controller
+    {
+        private readonly DataBaseContext _context;
+
+        public CountriesController(DataBaseContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Countries
+        public async Task<IActionResult> Index()
+        {
+              return _context.Contruies != null ? 
+                          View(await _context.Contruies.ToListAsync()) :
+                          Problem("Entity set 'DataBaseContext.Contruies'  is null.");
+        }
+
+        // GET: Countries/Details/5
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            if (id == null || _context.Contruies == null)
+            {
+                return NotFound();
+            }
+
+            var country = await _context.Contruies
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (country == null)
+            {
+                return NotFound();
+            }
+
+            return View(country);
+        }
+
+        // GET: Countries/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Countries/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Name,Id,CreateDate,ModifiedDate")] Country country)
+        {
+            if (ModelState.IsValid)
+            {
+                country.Id = Guid.NewGuid();
+                _context.Add(country);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(country);
+        }
+
+        // GET: Countries/Edit/5
+        public async Task<IActionResult> Edit(Guid? id)
+        {
+            if (id == null || _context.Contruies == null)
+            {
+                return NotFound();
+            }
+
+            var country = await _context.Contruies.FindAsync(id);
+            if (country == null)
+            {
+                return NotFound();
+            }
+            return View(country);
+        }
+
+        // POST: Countries/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, [Bind("Name,Id,CreateDate,ModifiedDate")] Country country)
+        {
+            if (id != country.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(country);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CountryExists(country.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(country);
+        }
+
+        // GET: Countries/Delete/5
+        public async Task<IActionResult> Delete(Guid? id)
+        {
+            if (id == null || _context.Contruies == null)
+            {
+                return NotFound();
+            }
+
+            var country = await _context.Contruies
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (country == null)
+            {
+                return NotFound();
+            }
+
+            return View(country);
+        }
+
+        // POST: Countries/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            if (_context.Contruies == null)
+            {
+                return Problem("Entity set 'DataBaseContext.Contruies'  is null.");
+            }
+            var country = await _context.Contruies.FindAsync(id);
+            if (country != null)
+            {
+                _context.Contruies.Remove(country);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CountryExists(Guid id)
+        {
+          return (_context.Contruies?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
--- ECommerce_Sat/DAL/Entities/Entity.cs
+++ ECommerce_Sat/DAL/Entities/Entity.cs
@@ -6,7 +6,12 @@ namespace ECommerce_Sat.DAL.Entities
     {
         [Key]
         public virtual Guid Id { get; set; }
+
+
+        [Display (Name =("Fecha de creación"))]
         public virtual String? CreateDate { get; set; }
+
+        [Display(Name = ("Fecha de modificación"))]
         public virtual String? ModifiedDate { get; set; }
     }
 }
